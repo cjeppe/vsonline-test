959e9189e49e9eabddd1376a837e3f238480093c
Rune W <10534982+rwejdling@users.noreply.github.com>
2020-02-14 11:23:06 +0100

    added httpie to install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -14,7 +14,7 @@ cat /etc/apt/sources.list.d/dotnetdev.list
 sudo apt-get update 
 
 # Install Azure Functions Core Tools
-sudo apt-get install -y azure-functions-core-tools
+sudo apt-get install -y azure-functions-core-tools httpie
 
 # Install Google chrome to use the auchenberg.vscode-browser-preview extension
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
